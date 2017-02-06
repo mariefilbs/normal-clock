f1a1e4ff85e4ebacdb3beb474818f67247b3ae90
Marie Filbey <patriciamariefilbey@gmail.com>
2017-02-06 14:53:00 -0500

    Working clock. Formatted correctly.

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+var clockDisplay = document.getElementById("clock");
+var backgroundDisplay = document.getElementById("clock-container")
 
-var clockDisplay = document.getElementsByClassName("clock");
-var backgroundDisplay = document.getElementsByClassName("clock-container");
+function showTime() {
+  var date = new Date();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+
+  if (seconds < 10) {seconds = "0" + seconds;}
+  if (minutes < 10) {minutes = "0" + minutes;}
+
+
+//Converting from military time
+  if (hours > 12) {hours = hours - 12; meridiem = "PM";}
+  if (hours === 0) {hours = 12;}
+  
+//needed to move this down to format AFTER changing to military time
+  if (hours < 10) {hours = "0" + hours;}
+
+  clockDisplay.innerHTML = (hours + ":" + minutes + ":" + seconds);
+
+}
+setInterval(showTime, 1000);
+showTime();
